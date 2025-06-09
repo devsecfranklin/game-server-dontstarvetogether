6288306c1da7c4b28bf757ef3ae765662b265a86
franklin <frank378@gmail.com>
2025-06-09 00:12:10 +0000

    game server save

---
+++ .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
@@ -0,0 +1,11 @@
+return {
+    ["workshop-3138571948"] = { enabled = true}, 
+    ["workshop-481765222"] = { enabled = true},
+    ["workshop-1185229307"] = { enabled = true},
+    ["workshop-3436020204"] = { enabled = true},
+    ["workshop-398858801"] = { enabled = true},
+    ["workshop-462434129"] = { enabled = true},
+    ["workshop-400409675"] = { enabled = true},
+    ["workshop-501387482"] = { enabled = true},
+    ["workshop-1077747217"] = { enabled = true}
+}
