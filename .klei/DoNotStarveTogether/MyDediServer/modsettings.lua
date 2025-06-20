573385d1af6556ec9d398d000133721772b2818b
devsecfranklin <franklin@bitsmasher.net>
2025-06-20 23:38:09 +0000

    game files

--- .klei/DoNotStarveTogether/MyDediServer/modsettings.lua
+++ .klei/DoNotStarveTogether/MyDediServer/modsettings.lua
@@ -1,6 +1,6 @@
-ForceEnableMod("3138571948")
-ForceEnableMod("481765222")
-ForceEnableMod("1185229307")
-ForceEnableMod("3436020204")
-ForceEnableMod("398858801")
+ForceEnableMod("workshop-3138571948")
+ForceEnableMod("workshop-481765222")
+ForceEnableMod("workshop-1185229307")
+ForceEnableMod("workshop-3436020204")
+ForceEnableMod("workshop-398858801")
 
