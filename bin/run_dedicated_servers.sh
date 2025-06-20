e15f90a20bb833c0a31c9d00405355f76e82f151
devsecfranklin <franklin@bitsmasher.net>
2025-06-20 03:19:53 +0000

    some files

--- bin/run_dedicated_servers.sh
+++ bin/run_dedicated_servers.sh
@@ -21,13 +21,13 @@ check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
 check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
 check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
 
-steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+# steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
 
 pushd "/home/dst/.local/share/Steam/steamapps/common/Don't Starve Together Dedicated Server/bin64"
 run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
-run_shared+=(-console)
+# run_shared+=(-console) # deprecated option
 run_shared+=(-cluster "$cluster_name")
-run_shared+=(-monitor_parent_process $$)
+# run_shared+=(-monitor_parent_process $$)
 
 "${run_shared[@]}" -shard Caves  | sed 's/^/Caves:  /' &
 "${run_shared[@]}" -shard Master | sed 's/^/Master: /'
