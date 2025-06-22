eef3a50147458882073e98601c00deddb364c9d2
devsecfranklin <franklin@bitsmasher.net>
2025-06-22 21:37:21 +0000

    server updates

--- .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
+++ .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
@@ -9,6 +9,23 @@ return {
     ["workshop-3425275001"]={ configuration_options={ }, enabled=true },
     ["workshop-3489027659"]={ configuration_options={ }, enabled=true },
     ["workshop-378160973"]={ configuration_options={ }, enabled=true },
-    ["workshop-66586835"]={ configuration_options={ }, enabled=true },
+    ["workshop-66586835"]={ configuration_options={ 
+        ANNOUNCE_OPEN = true , -- Announce
+	SAY_OPEN = true , -- Say
+	ANNOUNCE_CD = 20 , -- Announce Interval (min)
+	ANNOUNCE_ORDER = true , -- Announce Order
+	SAY_SHOWTIME = 3 , -- Say Show Time(sec)
+	SAY_DELAY = 1 , -- Say Delay (sec)
+	ANNOUNCE_TEXT = {
+		-- Random players:%pds days %pns name, the number of players:%nws world %ncs cave.
+		"we are streaming live at https://twitch.tv/s1y_b0rg",
+		"thanks for playing!",
+	} , -- Announce Message
+		ANNOUNCE_FIRSTJION_TEXT = "everybody act cool, %pds is here" , -- First Join Announce Text
+		SAY_FIRSTJION_TEXT = "welcome %pns. we are streaming live at https://twitch.tv/s1y_b0rg" , -- First Join Say Text
+		SAY_EVERYJION_TEXT = "welcome back %pns, nerd. time to die." , -- Every Join Say Text
+		SAY_DEATH_TEXT = "%pns u dead lol" , -- Death Say Text
+		SAY_RESURRECTION_TEXT = "%pns returns from beyond the grave!" , -- Resurrection Say Text
+    }, enabled=true },
     ["workshop-714712361"]={ configuration_options={ }, enabled=true },
 }
