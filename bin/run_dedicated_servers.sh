528c0a77a43260743991069857c808d0e8315f7f
devsecfranklin <franklin@bitsmasher.net>
2025-06-30 20:12:03 +0000

    even more debugging!

--- bin/run_dedicated_servers.sh
+++ bin/run_dedicated_servers.sh
@@ -12,13 +12,14 @@ LRED='\033[1;31m'
 LGREEN='\033[1;32m'
 LBLUE='\033[1;34m'
 CYAN='\033[0;36m'
+YELLOW=$(tput setaf 3)
 LPURP='\033[1;35m'
 NC='\033[0m' # No Color
 
 # --- Some config Variables ----------------------------------------
-BIN_DIR="/home/dst/.local/share/Steam/steamapps/common/Don\'t\ Starve\ Together/bin64"
+DST_SERVER_BASE_DIR="/home/franklin/workspace/game-server-dontstarvetogether/.local/share/Steam/steamapps/common/Don't Starve Together Dedicated Server"
 cluster_name="MyDediServer"
-dontstarve_dir="/home/dst/.klei/DoNotStarveTogether"
+dontstarve_dir="${HOME}/.klei/DoNotStarveTogether"
 
 # --- Helper Functions for Logging ---
 log_header() {
@@ -46,42 +47,43 @@ function check_for_file() {
   fi
 }
 
-function main() {
+function game_update() {
   log_header "Updating game server files from Steam"
   # steamcmd +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
   steamcmd +login anonymous +app_update 343050 validate +quit
+}
 
+function set_password(){
+  # make it so people need a password to get into server
+  log_info "sed the server password"
+  sed -i "s/cluster_password =/cluster_password = ${CLUSTER_TOKEN}/g" "${dontstarve_dir}/${cluster_name}/cluster.ini"
+}
+
+function main() {
   # Caves has a soft link to the same file
   log_header "Installing our custom server files"
-  cp /home/dst/saves/modoverrides.lua "${dontstarve_dir}/${cluster_name}/Master"
-  cp /home/dst/saves/dedicated_server_mods_setup.lua "${dontstarve_dir}/${cluster_name}"
-  cp /home/dst/saves/worldgenoverrides.lua "${dontstarve_dir}/$cluster_name/Master"
-  cp /home/dst/saves/worldgenoverrides.lua-caves "${dontstarve_dir}/${cluster_name}/Caves/worldgenoverrides.lua"
-
+  cp ${HOME}/saves/modoverrides.lua "${dontstarve_dir}/${cluster_name}/Master"
+  cp ${HOME}/saves/dedicated_server_mods_setup.lua "${dontstarve_dir}/${cluster_name}"
+  cp ${HOME}/saves/worldgenoverrides.lua "${dontstarve_dir}/$cluster_name/Master"
+  cp ${HOME}/saves/worldgenoverrides.lua-caves "${dontstarve_dir}/${cluster_name}/Caves/worldgenoverrides.lua"
   log_header "Copying cluster token into server files"
   echo "${CLUSTER_TOKEN}" >"${dontstarve_dir}/${cluster_name}/cluster_token.txt"
   check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
-  if [ ! -f "$dontstarve_dir/$cluster_name/cluster.ini" ]; then 
-    login_info "copying cluster.ini"
-    cp "/home/dst/saves/cluster.ini" "$dontstarve_dir/$cluster_name/cluster.ini"
-  fi
-  log_info "sed the secret token into the files"
-  sed -i "s/cluster_password =/cluster_password = ${CLUSTER_TOKEN}/g" "${dontstarve_dir}/${cluster_name}/cluster.ini"
+  cp "${HOME}/saves/cluster.ini" "$dontstarve_dir/$cluster_name/cluster.ini"
+  # set_password
   check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
   check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
   check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
+  log_warn "If any players cannot see the server in the list, update the game files from steam"
+  #game_update 
+  pushd "/home/dst/.local/share/Steam/steamapps/common/Don't Starve Together Dedicated Server/bin64"
+log_info "Start servers"
+run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
+run_shared+=(-cluster "$cluster_name")
+"${run_shared[@]}" -shard Caves  | sed 's/^/Caves:  /' &
+"${run_shared[@]}" -shard Master | sed 's/^/Master: /'
 
-  pushd "${BIN_DIR}" || fail "Unable to cd to bin dir"
-  run_shared=(./dontstarve_dedicated_server_nullrenderer_x64)
-  run_shared+=(-cluster "${cluster_name}")
-  log_header "start cave shard"
-  "${run_shared[@]}" -shard Caves | sed 's/^/Caves:  /' &
-  log_header "start master shard"
-  "${run_shared[@]}" -shard Master | sed 's/^/Master: /'
-  popd || fail "could not return successfully"
-
-  log_success "Ready to play!"
-
+log_success "Ready to play!"
 }
 
 main "$@"
