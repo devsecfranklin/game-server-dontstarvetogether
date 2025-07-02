3312873d30579b6f8cb9aaf87176d9396fba6167
devsecfranklin <franklin@bitsmasher.net>
2025-07-02 18:19:54 +0000

    server updates

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Don't Starve Together - Dedicated Server
 
 * To play on the Dont Starve Together server, start the game on Steam, then look for the server named &quot;wonderland&quot;
-* Streaming live to twitch.tv/s1y_b0rg 
+* Streaming live to twitch.tv/s1y\_b0rg 
 * Clone this repo onto your game server.
 * Create a dedicated user for the game, such as `dst`.
   * make the repo clone the home directory of the new dst user.
+
+![dst](https://github.com/devsecfranklin/game-server-dontstarvetogether/blob/main/images/dst-personajes.jpg)
 
 ## Install `direnv`
 
@@ -46,3 +48,22 @@ These two scripts set up the environment and start the game server.
 ./bin/run_dedicated_server.sh
 ```
 
+## Troubleshooting
+
+### Mod had no modworldgenmain.lua. Skipping.
+
+That means that mod (or mods) are no longer compatible with the current version of DST; or the
+files have become corrupt (which can happen even if others use the same mod without issue).
+
+You're going to need manually remove the mods from your game files (this happens quite often).
+This is an easy fix!:
+
+* Turn off auto-subscribe, if you have it on.
+* Unsubscribe from ALL mods, that are showing "workshop-xxxxxxx" for you, on the Steam Workshop page.
+* Go into your "Don't Starve Together" file/folder and manually delete ALL of those mods.
+  * manually delete ALL: "workshop-<number>" directories from your
+`steamapps\common\Don't Starve Together\mods` directory.
+
+
+Finally, you begin the process of adding those mods back in, one at a time, until you
+find the one(s) causing the issue.
--- saves/modoverrides.lua
+++ saves/modoverrides.lua
@@ -3,7 +3,11 @@
 -- SPDX-License-Identifier: MIT
 
 return {
-    ["workshop-378160973"]={
+    ["workshop-3138571948"]={
+        configuration_options={ },
+        enabled=true
+    },
+    ["workshop-3436020204"]={
         configuration_options={ },
         enabled=true
     },
@@ -11,30 +15,38 @@ return {
         configuration_options={ },
         enabled=true
     },
+    ["workshop-375859599"]={
+        configuration_options={ },
+        enabled=true
+    },
+    ["workshop-382177939"]={
+        configuration_options={ },
+        enabled=true
+    },
     ["workshop-2594707725"]={
         configuration_options={ },
         enabled=true
     },
-    ["workshop-665868350"] = { enabled = true,
-        configuration_options = {
-            ANNOUNCE_OPEN = true , -- Announce
-            SAY_OPEN = true , -- Say
-                        ANNOUNCE_CD = 3 , -- Announce Interval (min)
-                        ANNOUNCE_ORDER = true , -- Announce Order
-                        SAY_SHOWTIME = 3 , -- Say Show Time(sec)
-                        SAY_DELAY = 1 , -- Say Delay (sec)
-                        ANNOUNCE_TEXT = {
-                                -- Random players:%pds days %pns name, the number of players:%nws world %ncs cave.
-                                "you have no chance to survive make your time",
-                                "all your base are belong to  %pns" --  survive %pds days!",
-                        } , -- Announce Message
-                        ANNOUNCE_FIRSTJION_TEXT = "welcome to the struggle" , -- First Join Announce Text
-                        SAY_FIRSTJION_TEXT = "welcome to wonderland" , -- First Join Say Text
-                        SAY_EVERYJION_TEXT = "welcome to wonderland" , -- Every Join Say Text
-                        SAY_DEATH_TEXT = "i cant believe i died last night" , -- Death Say Text
-                        SAY_RESURRECTION_TEXT = "I LIVE" , -- Resurrection Say Text
-         }
-     }
-
-
+    ["workshop-665868350"]={
+        configuration_options={ },
+        enabled=true
+    },
+        ["workshop-1077747217"]={ configuration_options={
+        ANNOUNCE_OPEN = true , -- Announce
+	SAY_OPEN = true , -- Say
+	ANNOUNCE_CD = 20 , -- Announce Interval (min)
+	ANNOUNCE_ORDER = true , -- Announce Order
+	SAY_SHOWTIME = 3 , -- Say Show Time(sec)
+	SAY_DELAY = 1 , -- Say Delay (sec)
+	ANNOUNCE_TEXT = {
+		-- Random players:%pds days %pns name, the number of players:%nws world %ncs cave.
+		"we are streaming live at https://twitch.tv/s1y_b0rg",
+		"thanks for playing!",
+	} , -- Announce Message
+		ANNOUNCE_FIRSTJION_TEXT = "everybody act cool, %pds is here" , -- First Join Announce Text
+		SAY_FIRSTJION_TEXT = "welcome %pns. we are streaming live at https://twitch.tv/s1y_b0rg" , -- First Join Say Text
+		SAY_EVERYJION_TEXT = "welcome back %pns, nerd. time to die." , -- Every Join Say Text
+		SAY_DEATH_TEXT = "%pns u dead lol" , -- Death Say Text
+		SAY_RESURRECTION_TEXT = "%pns returns from beyond the grave!" , -- Resurrection Say Text
+    }, enabled=true },
 }
