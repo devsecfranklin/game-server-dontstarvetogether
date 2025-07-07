03fc0076536a5a8236c78ec9cad462734467cd2d
Franklin D. <2730246+devsecfranklin@users.noreply.github.com>
2025-07-07 11:53:15 -0600

    testing server patch (#37)
    * testing new patch
    
    * testing patch

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,24 +1,44 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# update July 2, 2025 franklin@bitsmasher.net
+# SPDX-FileCopyrightText: ©2025 franklin <franklin@bitsmasher.net>
 #
-#### User Variables; Set your preferences ####
+# SPDX-License-Identifier: MIT
 
+# v0.2 July 2, 2025 franklin@bitsmasher.net
+
+# --- Terminal Colors ---
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+LBLUE='\033[1;34m'
+# CYAN='\033[0;36m'
+LPURP='\033[1;35m'
+NC='\033[0m' # No Color
+
+# --- Helper Functions for Logging ---
+log_header() {
+	printf "\n${LPURP}# --- %s ${NC}\n" "$1"
+}
+
+log_info() { printf "${LBLUE}==>${NC} \e[1m%s\e[0m\n" "$1"; } # Using printf for Bold
+log_warn() { printf >&2 "${YELLOW}WARN:${NC} %s\n" "$1"; }
+log_success() { printf "${LGREEN}==>${NC} \e[1m%s\e[0m\n" "$1"; } # Using printf for Bold
+
+log_error() {
+	printf "${LRED}ERROR: %s${NC}\n" "$1" >&2
+	exit 1
+}
+
+# --- Some config Variables ----------------------------------------
 CLUSTER_NAME="MyDediServer"
 MASTER_NAME="Master"
 CAVE_NAME="Caves"
-
 CHECK_UPDATE_FREQ="20m"
-
 DONT_STARVE_CLUSTER_DIR="$HOME/.klei/DoNotStarveTogether"
 DONT_STARVE_DIR="/home/franklin/workspace/game-server-dontstarvetogether/.local/share/Steam/steamapps/common/Don't Starve Together Dedicated Server"
-
-#### Script Variables; Do not modify ####
-
 lv_file="/tmp/${CLUSTER_NAME}_latest_version"
 DONT_STARVE_BIN="./dontstarve_dedicated_server_nullrenderer_x64"
 
-#### Functions ####
+# --- Functions ####
 
 function needs_update() {
     klei_url="https://forums.kleientertainment.com/game-updates/dst/"
@@ -26,7 +46,7 @@ function needs_update() {
     if [[ -e $lv_file ]]; then
         current_version=$(head -n 1 $lv_file)
     fi
-    echo $latest_version > $lv_file
+    echo "${latest_version}" > $lv_file || log_error "unable to create latest version file"
     if [[ $current_version -lt $latest_version ]]; then
         echo "1"
     else
@@ -36,13 +56,8 @@ function needs_update() {
 
 function exists() {
     if [[ ! -e $1 ]]; then
-        failed "File/Dir not found: $1"
+        log_error "File/Dir not found: $1"
     fi
-}
-
-function failed() {
-    echo "Error: $@" >&2
-    exit 1
 }
 
 function command_m() {
@@ -70,42 +85,29 @@ function stop_server() {
     exit 1
 }
 
-#### Script routine ####
-
 trap stop_server 2
-
-# Check for missing files #
-
 exists "$DONT_STARVE_CLUSTER_DIR/$CLUSTER_NAME/cluster.ini"
 exists "$DONT_STARVE_CLUSTER_DIR/$CLUSTER_NAME/cluster_token.txt"
 exists "$DONT_STARVE_CLUSTER_DIR/$CLUSTER_NAME/$MASTER_NAME/server.ini"
 exists "$DONT_STARVE_CLUSTER_DIR/$CLUSTER_NAME/$CAVE_NAME/server.ini"
+needs_update > /dev/null 2>&1
 
-# Check for an update beforehand #
-needs_update 2>&1 >/dev/null
-
-while [[ true ]]; do
-
-    # Force an update #
-    mv "$DONT_STARVE_DIR/mods/dedicated_server_mods_setup.lua" "$DONT_STARVE_DIR/mods/dedicated_server_mods_setup.lua.bak"
+while true; do
+    mv "$DONT_STARVE_DIR/mods/dedicated_server_mods_setup.lua" "${DONT_STARVE_DIR}/mods/dedicated_server_mods_setup.lua.bak"
     echo "Start updating the game."
-    steamcmd +force_install_dir $DONT_STARVE_DIR +login anonymous +app_update 343050 validate +quit
-    mv "$DONT_STARVE_DIR/mods/dedicated_server_mods_setup.lua.bak" "$DONT_STARVE_DIR/mods/dedicated_server_mods_setup.lua"
+    steamcmd +force_install_dir ${DONT_STARVE_DIR} +login anonymous +app_update 343050 validate +quit
+    mv "$DONT_STARVE_DIR/mods/dedicated_server_mods_setup.lua.bak" "${DONT_STARVE_DIR}/mods/dedicated_server_mods_setup.lua"
 
-    # Check for DST binary #
     exists "$DONT_STARVE_DIR/bin64/dontstarve_dedicated_server_nullrenderer_x64"
 
-    # Run Shards #
-    cd "$DONT_STARVE_DIR/bin64"
-    echo "Starting ${CAVE_NAME}."
+    cd "${DONT_STARVE_DIR}/bin64" || log_error "fanny pack"
+    echo "Starting ${CAVE_NAME}"
     screen -d -m -S ${CLUSTER_NAME}_${CAVE_NAME} $DONT_STARVE_BIN -cluster $CLUSTER_NAME  -shard $CAVE_NAME
     echo "Starting ${MASTER_NAME}."
     screen -d -m -S ${CLUSTER_NAME}_${MASTER_NAME} $DONT_STARVE_BIN -cluster $CLUSTER_NAME  -shard $MASTER_NAME
-    # Checks for updates #
-    while [[ true ]]; do
-        
-        # Check for updates every 20 minutes #
-        sleep $CHECK_UPDATE_FREQ
+
+    while true; do
+        sleep $CHECK_UPDATE_FREQ # Check for updates every interval #
 
         # If there is an update, we will start the shutdown process #
         result=$(needs_update)
@@ -126,7 +128,7 @@ while [[ true ]]; do
 
     # We wait till the game shuts down #
     echo "Waiting for shards to shut down."
-    while [[ true ]]; do
+    while true; do
         if ! screen -list | grep -q ${CLUSTER_NAME}; then
             echo "Shards are down. Restarting."
             break
--- fyp_dist/src/loader/linux/signatures.h
+++
@@ -1,8 +0,0 @@
-#include "sigscan.h"
-
-const SIGNATURE SIGS_ALL[] = {
-    { (char*)"main_server",
-        (char*)"41 55 41 54 49 89 f4 55 89 fd bf 04 00 00 00 53 48 81 ec ?? 01 00 00 48 8d 74 24 20 48 c7 44 24 28 ff ff ff ff 48 c7 44 24 20 ff ff ff ff" },
-    { (char*)"main_client",
-        (char*)"41 57 41 56 41 55 41 54 49 89 f4 55 89 fd bf 04 00 00 00 53 48 81 ec b8 01 00 00 48 8d 74 24 10" }
-};
