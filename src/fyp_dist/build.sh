95983fd2c2251b118e10e8cab82554610a690997
Franklin D. <2730246+devsecfranklin@users.noreply.github.com>
2025-07-07 20:42:58 -0600

    testing GH action builds (#41)
    * testing GH action builds
    
    * testing build.sh
    
    * testing the build
    
    * testing the build
    
    * testing the build

--- src/fyp_dist/build.sh
+++ src/fyp_dist/build.sh
@@ -7,7 +7,7 @@
 # v0.1 07/04/2025 build script - ya ya pancho
 # v0.2 07/06/25 tweak-a-rooni franklin
 
-set -euo pipefail # Exit on error, exit on unset variables, fail if any command in a pipe fails.
+#set -euo pipefail # Exit on error, exit on unset variables, fail if any command in a pipe fails.
 IFS=$'\n\t'       # Preserve newlines and tabs in word splitting.
 
 # --- Some config Variables ----------------------------------------
@@ -17,7 +17,6 @@ FYP_OPTIONS="-DFYP_CCACHE=ON"
 CMAKE_OPTIONS="-DCMAKE_EXPORT_COMPILE_COMMANDS=True -DCMAKE_BUILD_TYPE=RelWithDebInfo"
 COMBINED_OPTIONS="$FYP_OPTIONS $CMAKE_OPTIONS"
 DEBIAN_PKG=(cmake ninja-build generate-ninja)
-#DOCUMENTATION=false
 KERNEL=$(uname -r)       # the kernel version
 MACHINE_TYPE=$(uname -m) # the machine type
 MY_DATE=$(date '+%Y-%m-%d-%H')
@@ -25,7 +24,6 @@ MY_OS=$(uname | tr '[:upper:]' '[:lower:]') # usually "linux"
 MY_UNAME=$(uname)
 OS_RELEASE="$(cat /etc/os-release | grep "^ID=" | cut -d"=" -f2)"
 PRIV_CMD="sudo"
-RAW_OUTPUT="/tmp/bootstrap_lab_${MY_DATE}.log"
 
 DIR="${PWD}"
 BUILD_DIR="${DIR}/build_linux"
@@ -191,17 +189,19 @@ function main() {
   check_python_version
   detect_os
   detect_hardware
-  cp toolchain/linux.cmake "${PWD}/build_linux"	
+
+  # cp "${PWD}/toolchain/linux.cmake" "${PWD}/build_linux"	
   make_that_dir "${BUILD_DIR}"
   install_deb_pkg
 
   log_header "Running cmake"
-  echo "Options chosen for this build:\n    ${COMBINED_OPTIONS}"
+  log_info  "Options chosen for this build:    ${COMBINED_OPTIONS}"
   log_header "Building for Linux"
-  /mnt/clusterfs/build/bin/cmake . "${COMBINED_OPTIONS}" -DCMAKE_TOOLCHAIN_FILE="${DIR}/toolchain/linux.cmake" -DCC="/usr/bin/gcc" -DCXX="/usr/bin/g++" -GNinja
+  /usr/bin/cmake "${COMBINED_OPTIONS}" -DCMAKE_TOOLCHAIN_FILE="${DIR}/toolchain/linux.cmake" -DCC="/usr/bin/gcc" -DCXX="/usr/bin/g++" -GNinja "${BUILD_DIR}"
+  /usr/bin/cmake -G Ninja .
   ninja
 
-  #echo "[$0] For Windows build using toolchain $toolchain_windows"
+  #log_info "[$0] For Windows build using toolchain $toolchain_windows"
   #cd build_windows;cmake .. $COMBINED_OPTIONS  -DCMAKE_TOOLCHAIN_FILE=$toolchain_windows; ninja
 }
 
