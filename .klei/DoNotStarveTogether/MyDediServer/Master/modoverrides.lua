c1055bf497a830f61e65bfe3441f5211288f5c2f
devsecfranklin <franklin@bitsmasher.net>
2025-07-27 19:13:09 +0000

    updates to mod overrides

--- .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
+++ .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
@@ -1,7 +1,5 @@
--- SPDX-FileCopyrightText: ©2025 franklin <franklin@bitsmasher.net>
---
--- SPDX-License-Identifier: MIT
-
+--- @file modoverrides.lua
+-- @brief This file determines which mods to load. It must be identical for each shard. 
 return {
     ["workshop-3138571948"]={
         configuration_options={ },
@@ -27,28 +25,8 @@ return {
         configuration_options={ },
         enabled=true
     },
-    ["workshop-2880198735"]={
+    ["workshop-1077747217"]={
         configuration_options={ },
         enabled=true
-    },
-    ["workshop-1077747217"] = {
-    enabled = true,
-    configuration_options = {
-        ANNOUNCEMENT_INTERVAL=20,
-        ANNOUNCEMENT_SWITCH = true,
-        ANNOUNCEMENT_TEXTS = {
-          "[SYSOPS] you have no chance to survive make your time",
-          "[SYSOPS] all your base are belong to us", --  survive %pds days!",
-          "[SYSOPS] server name is wonderland if you want tojoin the game",
-          "[SYSOPS] watch the game stream at https://www.twitch.tv/s1y_b0rg"
-        },
-        ANNOUNCE_FIRST_TIME_JOIN = "welcome to the struggle",
-        SPEAK_DURATION_SHOW = 5,
-        SPEAK_FIRST_TIME_JOIN = "welcome to wonderland",
-        SPEAK_EVERY_TIME_JOIN = "welcome to wonderland",
-        SPEAK_ON_DEATH = "i cant believe i died last night" , -- Death Say Text
-        SPEAK_ON__RESURRECT = "I LIVE" , -- Resurrection Say Text
-        SPEAK_SWITCH=true
-        }
     }
 }
